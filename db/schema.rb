4248b467e8242383b8debd00aeff8a59e94abcc4
Ranniery Jesuino de Oliveira <ranniery.jesuino@gmail.com>
2017-11-08 20:12:28 -0300

    db alterado de verdade

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171101165623) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "attendances", force: :cascade do |t|
     t.boolean "presenca"
     t.string "justificativa"
