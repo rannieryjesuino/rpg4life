40f94fa68db4f9062e4ff95820b7b348ae588c53
Ranniery Jesuino de Oliveira <ranniery.jesuino@gmail.com>
2017-12-26 18:15:25 -0300

    quase terminando o curso de railson

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,22 @@
 class PlayersController < ApplicationController
 
+  before_action pegar_parametros, only: [:create, :update]
+
+  def busca
+    @nome_a_buscar = params[:nome]
+    @jogadores = Player.where "nome like ?", "%#{@nome_a_buscar}%"
+  end
+
+  def edit
+    @jogador = Player.find params[:id]
+  end
+
+  def new
+    @jogador = Player.new
+  end
+
   def create
-    parametros = params.require(:player).permit :nome, :personagem
-    @jogador = Player.new parametros
+    @jogador = Player.new @parametros
     if @jogador.save
       flash[:notice] = "Jogador salvo com sucesso!"
       redirect_to players_url
@@ -11,8 +25,16 @@ class PlayersController < ApplicationController
     end
   end
 
-  def new
-    @jogador = Player.new
+  def update
+    id = params[:id]
+    @jogador = Player.find(id)
+    # Player.update(parametros[:id], :nome => parametros[:nome], :personagem => parametros[:personagem])
+    if @jogador.update @parametros
+      flash[:notice] = "Jogador atualizado com sucesso!"
+      redirect_to players_url
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -21,20 +43,14 @@ class PlayersController < ApplicationController
     redirect_to players_url
   end
 
-  def busca
-    @nome_a_buscar = params[:nome]
-    @jogadores = Player.where "nome like ?", "%#{@nome_a_buscar}%"
-  end
+# -------------------------------------------------------------------------------------------
+#                                    PRIVATE AREA
+# -------------------------------------------------------------------------------------------
 
-  def edit
-    @player = Player.find params[:id]
-  end
+  private
 
-  def update
-    parametros = params.require(:player).permit :nome, :personagem, :id
-    jogador = Player.find parametros[:id]
-    Player.update(parametros[:id], :nome => parametros[:nome], :personagem => parametros[:personagem])
-    redirect_to players_url
+  def pegar_parametros
+    @parametros = params.require(:player).permit :nome, :personagem
   end
 
 end
